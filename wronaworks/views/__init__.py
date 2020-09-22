1220d95d238c24ed3d3be6388382861219d7295f
tdkellar <tdkellar@umich.edu>
2020-09-21 23:25:44 -0400

    Finished layout for home page

--- wronaworks/views/__init__.py
+++ wronaworks/views/__init__.py
@@ -1,3 +1,7 @@
 from wronaworks.views.index import (
-    show_index_page
+    show_index_page,
+    show_experiences_page,
+    show_passions_page,
+    show_thoughts_page,
+    show_connect_page
 )
--- wronaworks/views/index.py
+++ wronaworks/views/index.py
@@ -3,6 +3,31 @@ import flask
 
 @wronaworks.app.route('/', methods=['GET'])
 def show_index_page():
-    context = {}
+    context = { 'page': 'home' }
     return flask.render_template("index.html", **context)
 # show_index_page
+
+
+@wronaworks.app.route('/experiences', methods=['GET'])
+def show_experiences_page():
+    context = { 'page': 'experiences' }
+
+    return flask.render_template("experiences.html", **context)
+
+@wronaworks.app.route('/passions', methods=['GET'])
+def show_passions_page():
+    context = { 'page': 'passions' }
+
+    return flask.render_template("passions.html", **context)
+
+@wronaworks.app.route('/thoughts', methods=['GET'])
+def show_thoughts_page():
+    context = { 'page': 'thoughts' }
+
+    return flask.render_template("thoughts.html", **context)
+
+@wronaworks.app.route('/connect', methods=['GET'])
+def show_connect_page():
+    context = { 'page': 'connect' }
+
+    return flask.render_template("connect.html", **context)
